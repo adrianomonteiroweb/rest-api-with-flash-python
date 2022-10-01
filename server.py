977dde507949e45e630db30b40f653734fc9dd1b
adriano <adrianomonteiroweb@gmail.com>
2022-10-01 10:04:01 -0300

    feat: implement post method

--- server.py
+++ server.py
@@ -6,3 +6,12 @@ db_connect = create_engine('sqlite:///exemplo.db')
 
 app = Flask(__name__)
 api = Api(app)
+
+class Users(Resource):
+  def get(self):
+    conn = db_connect.connect()
+
+    query = conn.execute("select * from user")
+    result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+
+    result jsonify(result)
